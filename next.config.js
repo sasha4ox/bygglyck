4ee2708a4262c90501722b807c9af6e19ca24d85
Oleksandr Lysytsia <oleksandr.lysytsia@rtl.nl>
2023-02-24 15:43:22 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.optimization.minimize = false;
+  }
 }
 
 module.exports = nextConfig
