b08d65209045220bebeb0469e1468dc9a24feb10
Fox <sasha4ox@gmail.com>
2025-04-30 17:53:40 +0300

    changed photo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "955df82734ea431e"; }
+/******/ 		__webpack_require__.h = function() { return "1505d54311ec4cca"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
